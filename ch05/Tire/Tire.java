959f962c1de0acacf8adeff93220766442cca586
ChaeYun430 <marchlcy@gmail.com>
2025-06-01 01:19:46 +0900

    Tire.java 업데이트

--- ch05/Tire/Tire.java
+++ ch05/Tire/Tire.java
@@ -13,10 +13,10 @@ public class Tire {
 	public boolean roll() {
 		if (accRotation < macRotation) {
 			accRotation++;
-			System.out.println("남은 회전 수" + (macRotation - this.accRotation));
+			System.out.println("남은 타이어 수명: " + (macRotation - this.accRotation) + "마일리지");
 			return true;
 		}else {
-			System.out.println(location + " 타이어가 펑크 경고");
+			System.out.println(location + " 타이어 펑크 경고");
 			return false;
 		}
 	}
