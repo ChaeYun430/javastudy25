e1668ec34499ef9ad35b6321dfc7a9313ff3dc4d
ChaeYun430 <marchlcy@gmail.com>
2025-06-01 15:24:05 +0900

    CarExam.java 업데이트

--- ch05/Tire/CarExam.java
+++ ch05/Tire/CarExam.java
@@ -9,17 +9,40 @@ public class CarExam {
 	
 	public static void main(String[] args) {
 		Car myCar = new Car();
+
 		boolean run = true;
 		while (run) {
+		for(int i = 0; i < 4; i++){
+		if(i == myCar.run()){
+		System.out.println(myCar.tire[i].location + " 타이어 교체");
+		
+			while(true){
+			System.out.println("어느 타이어로 교체하시겠습니까?");
+			System.out.prinln("1. 금호타이어 | 2. 한국 타이어");
+			System.out.print(">>>");
+			String select = scStr.next();
+			switch(select){
+			case "1":
+			myCar.tire[i] = new ghTire(20, myCar.tire[i].location);
+			break;
+			case "2":
+			myCar.tire[i] = new hgTire(20, myCar.tire[i].location);
+			break;
+			default :
+			System.out.println("다시 한 번 선택해주세요.");
+			}//while(true)
+		}//if(i == myCar.run())
+}//for 교체여부 결정
+
 			String[] menus = {"1. 직진", "2. 좌회전", "3. 우회전", "4. 후진", "정지"};
 			for (int i = 0; i < menus.length; i++) {
 				System.out.println(menus[i]);
 			}
 			System.out.print(">>>");
-			String select = scStr.next();
+			select = scStr.next();
 			switch (select) {
 			case "1":
-				run = myCar.leftFront.roll();
+				run = myCar.tire[0].roll();
 				run = myCar.leftBack.roll();
 				run = myCar.rightFront.roll();
 				run = myCar.rightBack.roll();
@@ -40,19 +63,11 @@ public class CarExam {
 				break;
 			case "5":
 				run = false;
-		
+		 	System.out.println("정지합니다.");
 			default:
 				System.out.println("이전 메뉴로 돌아갑니다.");
 			}
-		}
 
-	}
-	static void inspect(boolean tf, Car car) {
-		if (tf ==true) {
-			System.out.println("타이어 정상!");
-		}
-		System.out.println("타이어 교체");
-		car.change(car);
-	}
-
-}
+		}//while(run)
+	}//main method
+}//class
